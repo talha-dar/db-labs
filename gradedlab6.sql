d304035bcae1fc47d15abbb0344b6be0f8be5bbf
Muhammad Talha Dar <tdtalhada@gmail.com>
2020-11-18 15:03:11 +0500

    commit glab6 complete

--- gradedlab6.sql
+++ gradedlab6.sql
@@ -1,23 +1,19 @@
+-- import database from file world.sql
 
-
-
+use world;
 
 -- task1
+select distinct Region from country where LifeExpectancy<50;
 
 -- task2
+select Language from countrylanguage where isOfficial='F' and percentage between 50 and 80 ;
 
 -- task3
+select Name from country where Name like('C_i%'); 
 
 -- task4
+create table temp(select Code, LocalName from country where LocalName not like ('congo'));
+select Code, LocalName from temp where LocalName not like ('chile');
 
 -- task5
-
--- task6
-
--- task7
-
--- task8
-
--- task9
-
--- task10
+select Code, Language from country, countryLanguage where country.Code=countrylanguage.CountryCode and IndepYear like('198_');
